20fba526392a66aa65a207f724639e71b10a7fdd
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-04-28 14:41:55 +0200

    add milestone 1

---
+++ milestone1/js/script.js
@@ -0,0 +1,41 @@
+// MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+// stampo con Dot notation
+for (let i = 0; i < team.length; i++) {
+    const informationElement = team[i];
+    console.log("nome:", informationElement.name, "ruolo:", informationElement.role, "image:", informationElement.image);
+}
